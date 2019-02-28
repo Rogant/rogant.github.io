b895e0d36acb0828db2fca45db8a09caadbac88f
Andres Vargas <a.vargas@globant.com>
2019-02-28 13:55:36 -0500

    add zeit support

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Under construction... <span className="blinking-cursor">|</span>
+            Under construction... <span className="App-blinking">|</span>
           </p>
           <a
             className="App-link"
