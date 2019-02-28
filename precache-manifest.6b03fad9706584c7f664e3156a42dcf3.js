5e9c8eb02521eac443961619d61b0a95cab4bd75
Andres Vargas <a.vargas@globant.com>
2019-02-28 12:08:04 -0500

    Updates

---
+++ precache-manifest.6b03fad9706584c7f664e3156a42dcf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rogant.github.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "14deaffec0a1b2305845",
+    "url": "/rogant.github.io/static/js/runtime~main.14deaffe.js"
+  },
+  {
+    "revision": "c681d9013c5aa31b338e",
+    "url": "/rogant.github.io/static/js/main.c681d901.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/rogant.github.io/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "c681d9013c5aa31b338e",
+    "url": "/rogant.github.io/static/css/main.489847a9.chunk.css"
+  },
+  {
+    "revision": "6d31f4c146ae246c37ac850d6d248fba",
+    "url": "/rogant.github.io/index.html"
+  }
+];
